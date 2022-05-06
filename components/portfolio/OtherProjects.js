1b54e450090437a46978819f6aa6ff71eb42337d
Jia Chen <jdchen@webmd.net>
2022-05-06 18:25:02 -0400

    other projects: open in new tab if it is external link

--- components/portfolio/OtherProjects.js
+++ components/portfolio/OtherProjects.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const projects = [
-    { name: 'Blog', link: 'https://jiachen.netlify.com/blog', gitLink: 'https://github.com/davidchenbest/portfolio2' },
-    { name: 'Gallery', link: 'https://jiachen.netlify.com/gallery', gitLink: 'https://github.com/davidchenbest/portfolio2' },
+    { name: 'Blog', link: '/blog', gitLink: 'https://github.com/davidchenbest/portfolio2' },
+    { name: 'Gallery', link: '/gallery', gitLink: 'https://github.com/davidchenbest/portfolio2' },
     { name: 'Daily Wallpaper', gitLink: 'https://github.com/davidchenbest/daily-wallpaper' },
     { name: 'Flashcard', link: 'https://flashcard-jia.web.app/', gitLink: 'https://github.com/davidchenbest/flashcards-app' },
 ]
@@ -18,9 +19,14 @@ export default function OtherProjects() {
         <div className='other-projects' onClick={(e) => otherProject(e)}>
             <h2 className='second-title'>Other Projects</h2>
             {projects.map((project, i) => <div className='otherProject' key={i}>
-                <a rel="noopener noreferrer" target="_blank" href={project.link}>
-                    <span>{project.name}</span>
-                </a>
+                {project.link ?
+                    <Link href={project.link}>
+                        <a rel="noopener noreferrer" target={!project.link.startsWith('/') && '_blank'} >
+                            <span>{project.name}</span>
+                        </a>
+                    </Link>
+                    : <span>{project.name}</span>
+                }
                 <a rel="noopener noreferrer" target="_blank" href={project.gitLink} id="gitIcon" >
                     <Image alt='github' height={25} width={25} src={require('../../images/github.png')} />
                 </a>
