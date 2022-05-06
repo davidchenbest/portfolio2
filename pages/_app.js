4c2a242b61cc6b5772a1212cdb95b4058e24c526
Jia Chen <jdchen@webmd.net>
2022-05-06 00:09:30 -0400

    move google font link to _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,9 @@ import '../styles/gallery.css'
 import '../styles/navigator.css'
 import '../styles/photosCon.css'
 import Nav from '../components/nav'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
   return (<>
-    <Head>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    </Head>
     <Nav />
     <Component {...pageProps} />
   </>)
