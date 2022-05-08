12d6dabca6fd29f25d2bfe3ad85bc6681cbb79af
Jia Chen <jdchen@webmd.net>
2022-05-07 23:43:37 -0400

    remove console log through next.config, uninstall babel-plugin-transform-remove-console

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const { BLOG_WORD_LIMIT, TITLE_NAME, IMG_UNOPTIMIZE, SOCKET_URL } = process.env
+const { BLOG_WORD_LIMIT, TITLE_NAME, IMG_UNOPTIMIZE, SOCKET_URL, NODE_ENV } = process.env
 
 const nextConfig = {
   reactStrictMode: true,
@@ -12,7 +12,10 @@ const nextConfig = {
     TITLE_NAME,
     IMG_UNOPTIMIZE,
     SOCKET_URL
-  }
+  },
+  compiler: {
+    removeConsole: NODE_ENV === 'production',
+  },
 }
 
 module.exports = nextConfig
