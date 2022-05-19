1292be09de5d62a6543e867d6462cc4ff1d26569
Jia Chen <jdchen@webmd.net>
2022-05-19 19:42:56 -0400

    correct sierpinski spelling

--- pages/concepts/sierpinski.js
+++ pages/concepts/sierpinski.js
@@ -1,7 +1,7 @@
 import Headers from "../../components/Headers";
 import App from "../../components/sierpinski/App";
 
-export default function Sierpinki() {
+export default function Sierpinski() {
     return <>
         <Headers name='sierpinski' />
         <App />
