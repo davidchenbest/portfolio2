70d33ba9b901089575089ff809f6f13e00005e07
Jia Chen <jdchen@webmd.net>
2022-05-19 20:25:15 -0400

    split projects into categories

--- components/portfolio/OtherProjects.js
+++ components/portfolio/OtherProjects.js
@@ -4,12 +4,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 const { IMG_UNOPTIMIZE } = process.env
 
-const projects = [
-    { name: 'Blog', link: '/projects/blog', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/blog' },
-    { name: 'Gallery', link: '/projects/gallery', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/gallery' },
-    { name: 'Video Chat', link: '/tools/videoroom', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/webrtc' },
-    { name: 'Daily Wallpaper', gitLink: 'https://github.com/davidchenbest/daily-wallpaper' },
-    { name: 'Flashcard', link: 'https://flashcard-jia.web.app/', gitLink: 'https://github.com/davidchenbest/flashcards-app' },
+const categories = [
+    {
+        name: 'Projects', apps: [
+            { name: 'Blog', link: '/projects/blog', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/blog' },
+            { name: 'Gallery', link: '/projects/gallery', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/gallery' },]
+    },
+    {
+        name: 'Tools', apps: [
+            { name: 'Video Chat', link: '/tools/videoroom', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/webrtc' },
+            { name: 'Daily Wallpaper', gitLink: 'https://github.com/davidchenbest/daily-wallpaper' },
+            { name: 'Flashcard', link: 'https://flashcard-jia.web.app/', gitLink: 'https://github.com/davidchenbest/flashcards-app' },]
+    },
+    {
+        name: 'Concepts', apps: [
+            { name: 'Sierpinski', link: '/concepts/sierpinski', gitLink: 'https://github.com/davidchenbest/portfolio2/tree/master/components/sierpinski' },
+        ]
+    }
 ]
 
 export default function OtherProjects() {
@@ -18,22 +29,30 @@ export default function OtherProjects() {
         if (element) element.click()
     }
     return (
-        <div className='other-projects' onClick={(e) => otherProject(e)}>
-            <h2 className='second-title'>Other Projects</h2>
-            {projects.map((project, i) => <div className='otherProject' key={i}>
-                {project.link ?
-                    <Link href={project.link}>
-                        <a rel="noopener noreferrer" target={!project.link.startsWith('/') ? '_blank' : null} >
-                            <span>{project.name}</span>
-                        </a>
-                    </Link>
-                    : <span>{project.name}</span>
-                }
-                <a rel="noopener noreferrer" target="_blank" href={project.gitLink} id="gitIcon" >
-                    <Image alt='github' height={25} width={25} src={require('../../images/github.png')} unoptimized={IMG_UNOPTIMIZE} />
-                </a>
-            </div>)}
+        <>
+            <h2 className='second-title'>Other Apps</h2>
+            <div className='other-apps' onClick={(e) => otherProject(e)}>
 
-        </div>
+                {categories.map(category => <div key={category.name}>
+                    <h3>{category.name}</h3>
+                    <div className='category'>
+                        {category.apps.map((app) => <div className='otherApp' key={app.name}>
+                            {app.link ?
+                                <Link href={app.link}>
+                                    <a rel="noopener noreferrer" target={!app.link.startsWith('/') ? '_blank' : null} >
+                                        <span>{app.name}</span>
+                                    </a>
+                                </Link>
+                                : <span>{app.name}</span>
+                            }
+                            <a rel="noopener noreferrer" target="_blank" href={app.gitLink} id="gitIcon" >
+                                <Image alt='github' height={25} width={25} src={require('../../images/github.png')} unoptimized={IMG_UNOPTIMIZE} />
+                            </a>
+                        </div>)}
+                    </div>
+                </div>)}
+
+            </div>
+        </>
     )
 }
