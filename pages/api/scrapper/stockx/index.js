1b7e8365c6afb7053888a50ad442d7e0e8f72913
davidchenbest <53093261+davidchenbest@users.noreply.github.com>
2023-07-18 20:53:33 +0000

    update insert product to track

--- pages/api/scrapper/stockx/index.js
+++ pages/api/scrapper/stockx/index.js
@@ -1,14 +1,18 @@
 import MongoConnection from "lib/mongoConnection"
+import fetch from "node-fetch"
 
 export default async function handler(req, res) {
+    const { product, size } = req.query
+    if (!product || !size) return res.status(500).json('give product name and size')
+    const exist = await productExist(`https://stockx.com/${product}`)
+    if (!exist) return res.status(500).json('give valid product')
     const mongo = new MongoConnection('scrapper', 'producttracking')
     try {
         const connection = await mongo.getConnection()
 
         //add
-        const product = 'nike-dunk-low-retro-white-black-2021'
         const existInDB = await connection.count({ name: product }, { limit: 1 })
-        if (!existInDB) await connection.insertOne({ name: product, date: new Date() })
+        if (!existInDB) await connection.insertOne({ name: product, size, date: new Date() })
 
         res.status(200).json({ existInDB })
     } catch (error) {
@@ -20,4 +24,9 @@ export default async function handler(req, res) {
             mongo.closeConnection(),
         ])
     }
+}
+
+async function productExist(url) {
+    const res = await fetch(url)
+    return !(res.status + '').startsWith(4)
 }
