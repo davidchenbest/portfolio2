a56b8250015649cf22be0e59fb0bf90f16ff100b
davidchenbest <53093261+davidchenbest@users.noreply.github.com>
2023-07-23 23:00:52 +0000

    instaGallery

--- pages/projects/gallery.js
+++ pages/projects/gallery.js
@@ -1,13 +1,15 @@
 import Gallery from "../../components/gallery/Gallery"
 import MongoConnection from "../../lib/mongoConnection"
 import Headers from "../../components/Headers"
+import InsaPosts from "components/gallery/InstaPost"
 
 
-export default function GalleryPage({ data }) {
+export default function GalleryPage({ albums, instaPosts }) {
     return (
         <>
             <Headers name='gallery' />
-            <Gallery data={data} />
+            <InsaPosts albums={instaPosts} />
+            <Gallery albums={albums} />
         </>
     )
 }
@@ -15,12 +17,33 @@ export default function GalleryPage({ data }) {
 export async function getStaticProps(context) {
     const mongo = new MongoConnection('blog', 'photofolders')
     const connection = await mongo.getConnection()
-    const data = await connection.find().toArray()
-    data.forEach(folder => {
+    const albums = await connection.find().toArray()
+    albums.forEach(folder => {
         folder.photos.sort((a, b) => b.date - a.date)
     });
     mongo.closeConnection()
+    const instaPosts = await getInstaPosts()
     return {
-        props: { data: JSON.parse(JSON.stringify(data)) }, // will be passed to the page component as props
+        props: { ...JSON.parse(JSON.stringify({ albums, instaPosts })) }, // will be passed to the page component as props
+    }
+}
+
+async function getInstaPosts() {
+    const mongo = new MongoConnection('blog', 'insta')
+    try {
+        const connection = await mongo.getConnection()
+        const posts = await connection.find().toArray()
+        for (const post of posts) {
+            post.photos = post.photos.map((p, i) => ({ photoLink: p, _id: i }))
+        }
+        return posts
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+    finally {
+        await Promise.allSettled([
+            mongo.closeConnection(),
+        ])
     }
 }
