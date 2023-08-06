4e92f43358ada0811079c527cf4d28e42ecd1b8b
davidchenbest <53093261+davidchenbest@users.noreply.github.com>
2023-08-06 18:52:48 +0000

    minPrice

--- pages/api/scrapper/stockx/index.js
+++ pages/api/scrapper/stockx/index.js
@@ -2,7 +2,7 @@ import MongoConnection from "lib/mongoConnection"
 import fetch from "node-fetch"
 
 export default async function handler(req, res) {
-    const { product, size } = req.query
+    const { product, size, minPrice } = req.query
     if (!product || !size) return res.status(500).json('give product name and size')
     const exist = await productExist(`https://stockx.com/${product}`)
     if (!exist) return res.status(500).json('give valid product')
@@ -11,8 +11,10 @@ export default async function handler(req, res) {
         const connection = await mongo.getConnection()
 
         //add
+        const obj = { name: product, size, date: new Date() }
+        if (minPrice) obj.minPrice = minPrice
         const existInDB = await connection.count({ name: product }, { limit: 1 })
-        if (!existInDB) await connection.insertOne({ name: product, size, date: new Date() })
+        if (!existInDB) await connection.insertOne(obj)
 
         res.status(200).json({ existInDB })
     } catch (error) {
