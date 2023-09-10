4406621d4a0ffaa5314837e3defe66d549261a6e
davidchenbest <53093261+davidchenbest@users.noreply.github.com>
2023-09-10 04:12:01 +0000

    stats for row, col

--- pages/scrapper/[id].js
+++ pages/scrapper/[id].js
@@ -1,48 +1,75 @@
 import MongoConnection from 'lib/mongoConnection.mjs';
 import { ObjectId } from 'mongodb';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
-function calLowHigh(product) {
-    return product.prices.map(p => {
-        const prices = []
-        for (const { price } of p.price) {
-            const num = +(price.replace(/[^0-9.]/g, ""))
-            if (num > 0) prices.push(num)
+function calLowHigh(prices) {
+    const low = Math.min(...prices)
+    const high = Math.max(...prices)
+    return { low, high }
+}
+
+function calAverage(prices) {
+    const averages = new Array(prices.length)
+    for (let i = 0; i < prices.length; i++) {
+        const len = prices[i].length
+        averages[i] = (prices[i].reduce((total, price) => total + price, 0) / len).toFixed(2)
+    }
+    return averages
+}
+
+function colPrices(product) {
+    const cols = Array.from({
+        length: product.prices[0]?.price.length
+    }, () => new Array());
+    for (const p of product.prices) {
+        for (let i = 0; i < p.price.length; i++) {
+            let { price } = p.price[i];
+            price = +(price.replace(/[^0-9.]/g, ""))
+            if (price > 0) cols[i].push(price)
         }
-        const low = Math.min(...prices)
-        const high = Math.max(...prices)
-        return { low, high }
-    })
+    }
+    return cols
+}
+
+function rolPrices(p) {
+    const prices = []
+    for (const { price } of p.price) {
+        const num = +(price.replace(/[^0-9.]/g, ""))
+        if (num > 0) prices.push(num)
+    }
+    return prices
 }
 
 export default function Product({ product }) {
-    const averagesCol = useMemo(() => {
-        const avgs = Array.from({
+    const [row, setRow] = useState(-1)
+    const [col, setCol] = useState(-1)
+    const statsCol = useMemo(() => {
+        const cols = Array.from({
             length: product.prices[0]?.price.length
-        }, () => new Array());
-        for (const p of product.prices) {
-            for (let i = 0; i < p.price.length; i++) {
-                let { price } = p.price[i];
-                price = +(price.replace(/[^0-9.]/g, ""))
-                if (price > 0) avgs[i].push(price)
-            }
+        }, () => ({}));
+        const columnPrices = colPrices(product)
+        const averages = calAverage(columnPrices)
+        const lowHigh = columnPrices.map(col => calLowHigh(col))
+        console.log(lowHigh);
+        for (let i = 0; i < cols.length; i++) {
+            cols[i].average = averages[i]
+            cols[i].low = lowHigh[i].low
+            cols[i].high = lowHigh[i].high
         }
-
-        for (let i = 0; i < avgs.length; i++) {
-            const len = avgs[i].length
-            avgs[i] = (avgs[i].reduce((total, price) => total + price, 0) / len).toFixed(2)
-        }
-        return avgs
+        return cols
     }, [product])
 
     const statsRow = useMemo(() => {
         const combine = Array.from({
             length: product.prices.length
         }, () => ({}))
-        const lowHigh = calLowHigh(product)
+        const rowPrices = product.prices.map(row => rolPrices(row))
+        const lowHigh = rowPrices.map(row => calLowHigh(row))
+        const averages = calAverage(rowPrices)
         for (let i = 0; i < combine.length; i++) {
             combine[i].low = lowHigh[i].low
             combine[i].high = lowHigh[i].high
+            combine[i].average = averages[i]
 
         }
         return combine
@@ -57,24 +84,28 @@ export default function Product({ product }) {
                         <p>{size}:</p>
                     </td>)}</tr>
                 <tr>
-                    <td></td>
-                    {averagesCol.map((avg) => <td key={avg} className='border border-slate-700 px-3 py-2'>
-                        <p>{avg}</p>
+                    <td>Average</td>
+                    {statsCol.map(({ average }) => <td key={average} className='border border-slate-700 px-3 py-2'>
+                        <p>{average}</p>
                     </td>)}</tr>
             </thead>
             <tbody>
-                {product.prices.map(({ price, date }, i) => <tr key={date} className='hover:'>
+                {product.prices.map(({ price, date }, i) => <tr
+                    onMouseEnter={() => setRow(i)}
+                    key={date} className='hover:'>
 
                     <td>{new Date(date).toISOString()}</td>
 
-                    {price.map(({ size, price }) => {
+                    {price.map(({ size, price }, j) => {
                         let extraClass = ' '
                         const p = +(price.replace(/[^0-9.]/g, ""))
                         const isHigh = p === statsRow[i].high
                         const isLow = p === statsRow[i].low
                         if (isHigh) extraClass += 'text-green-500 font-bold'
                         else if (isLow) extraClass += 'text-red-500 font-bold'
-                        return <td key={size} className='border border-slate-700 px-3 py-2'>
+                        return <td
+                            onMouseEnter={() => setCol(j)}
+                            key={size} className='border border-slate-700 px-3 py-2'>
                             <p className={extraClass}>{price}</p>
                         </td>
                     })}
@@ -82,6 +113,20 @@ export default function Product({ product }) {
             </tbody>
 
         </table>
+        <section className='fixed bottom-0 right-0 p-2 bg-slate-400'>
+            {row > -1 && <div className='flex gap-2'>
+                <h3>Row: {row}</h3>
+                <p>average: {statsRow[row]?.average}</p>
+                <p>high: {statsRow[row]?.high}</p>
+                <p>low: {statsRow[row]?.low}</p>
+            </div>}
+            {col > -1 && <div className='flex gap-2'>
+                <h3>Col: {col}</h3>
+                <p>average: {statsCol[col]?.average}</p>
+                <p>high: {statsCol[col]?.high}</p>
+                <p>low: {statsCol[col]?.low}</p>
+            </div>}
+        </section>
     </>
 }
 
